8bed7e5b04d235120840ebd5fbd82dee7e4e5e26
batonbl4 <56043798+batonbl4@users.noreply.github.com>
2019-10-01 21:56:39 +0400

    Add files via upload

---
+++ CMakeProject1.h
@@ -0,0 +1,36 @@
+﻿// CMakeProject1.h : включаемый файл для стандартных системных включаемых файлов
+// или включаемые файлы для конкретного проекта.
+
+#pragma once
+
+#include <iostream>
+#include <thread>
+#include <random>
+#include <mutex>
+#include <condition_variable>
+
+class DataControl
+{
+public:
+	DataControl(int sensorsCount)
+	{
+		sensors = sensorsCount;
+		sensorsData = new int[sensors];
+	}
+	~DataControl()
+	{
+		delete[] sensorsData;
+	}
+	void CreateSensorsThread();
+	
+	
+private:
+	std::mutex myMutex;
+	std::condition_variable myVar;
+	int* sensorsData;
+	int sensors;
+	void DataThread(int* const dataArray, int id);
+	void ViewErrorData(int* const dataArray, int size);
+};
+
+// TODO: установите здесь ссылки на дополнительные заголовки, требующиеся для программы.
